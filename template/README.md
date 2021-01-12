d9a4aba5436740de99916797c30b2ccf93b43de2
Kathleen Stickel <kstick9210@outlook.com>
2021-01-12 15:48:06 -0600

    create template and update package.json

---
+++ template/README.md
@@ -0,0 +1,1 @@
+# kstick React App template
---
+++ template/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React App by</h1>
+      <h1 className="cinque">&lt; Cinque Web Development /&gt;</h1>
+    </div>
+  )
+}
