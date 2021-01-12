d883ef80799fe7009f0fb49d843f45ba709ee762
Kathleen Stickel <kstick9210@outlook.com>
2021-01-12 15:31:57 -0600

    style App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>React App by</h1>
-      <h1 className="cinque">&lt; Cinque Web Development /&rt;</h1>
+      <h1 className="cinque">&lt; Cinque Web Development /&gt;</h1>
     </div>
   )
 }
